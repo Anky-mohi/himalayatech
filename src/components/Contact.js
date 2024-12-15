12375fab8311e168d820260e70ca5ec39b3a150e
ankymohi <ak862041311@gmail.com>
2024-12-15 06:23:26 -0800

    Initial commit

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -186,7 +186,7 @@ const Contact = () => {
               </div>
               <div class="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
 
-                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-registration/draw1.webp"
+                <img src="./bathroom-sign-wall-fotor-20240116133436.jpg"
                   class="img-fluid" alt="Sample image"></img>
 
               </div>
